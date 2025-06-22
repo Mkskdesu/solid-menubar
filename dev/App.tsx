f4d7345c2d455e9328d5faa73b394a2ade895d13
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2025-06-22 12:12:32 +0900

    initial commit

---
+++ dev/App.tsx
@@ -0,0 +1,12 @@
+import type {Component} from 'solid-js'
+import style from './App.module.scss'
+
+const App: Component = () => {
+    return (
+        <div class={style.App}>
+
+        </div>
+    )
+}
+
+export default App
---
+++ dev/index.tsx
@@ -0,0 +1,6 @@
+import { render } from 'solid-js/web'
+import './style.scss'
+
+import App from './App'
+
+render(() => <App />, document.getElementById('root')!)
