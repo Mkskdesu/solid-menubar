3e946137c0b6bf6fc6a0e7c524a55dfab0139ab8
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2025-06-22 22:15:43 +0900

    add readme and examples

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # solid-menubar
+
+[solidjs](https://github.com/solidjs/solid) 向けのメニューバーを作成するコンポーネントライブラリ。
+
+A component library to create app-menu bars for solidjs.
+
+スクリーンショット / Screenshot:
+
+![img.png](docs/img.png)
+ライトテーマ /Light theme :
+
+![img.png](docs/light.png)
+
+# 目次 / contents
++ インストール / installation
++ 使い方 / usage
++ 型 / type definition
+
+# インストール / installation
+`npm i solid-menubar`
+
+# 使い方 / usage
+### インポート / import
+```ts
+import MenuBar from "solid-menubar";
+```
+
+### コンポーネントを利用する / use components
+
+詳細な例は[dev/App.tsx](./dev/App.tsx)をご覧ください。 / Detailed examples are in `dev/App.tsx`.
+
+```tsx
+const YourComponent = () => {
+    return (
+        <MenuBar.Container config={config}>
+            <MenuBar.Group title={"Tab1"}>
+                <MenuBar.ListItem onClick={()=>console.log("You clicked list!")}>
+                    List1
+                </MenuBar.ListItem>
+                ...
+            </MenuBar.Group>
+        </MenuBar.Container>
+    )
+}
+
+```
+
+# 型 / type definition
+型定義は[src/types.d.ts](./src/types.d.ts)にあります。 / Type definition file is in `src/types.d.ts`.
+## Container -> config
+
+```ts
+interface IConfig {
+    //規定のcssを無効化します。 / Disable builtin styles.
+    noStyle?: Boolean //default:false
+    //規定cssのテーマ / theme for builtin styles.
+    theme?: "light" | "dark" //default: light
+    listItem?: {
+        //<MenuBar.ListItem type={"checkbox"}> チェックボックスのチェックマークをオーバーライドします.
+        //Override checkmark icon for <MenuBar.ListItem type={"checkbox"}>
+        checkMarkOverride?: JSXElement
+    }
+    subGroup?:{
+        //サブメニューの矢印を表示するかを設定します。 コンポーネントのicon propによるオーバーライドが優先されます。
+        //Whether to display the submenu arrow icons. Overriding with icon prop of component is prioritized.
+        showArrow?:boolean //default:true
+        arrowIconOverride?: JSXElement
+    }
+}
+```
+
+
--- dev/App.tsx
+++ dev/App.tsx
@@ -1,10 +1,78 @@
-import type {Component} from 'solid-js'
+import {Component, createSignal} from 'solid-js'
 import style from './App.module.scss'
+import MenuBar from "../dist/index.jsx";
+import "../dist/index.css"
+
+//Gear icon from microsoft/vscode-icons
+const GearIcon: Component = () => {
+    return (
+        <svg fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="1em"
+             width="1em" style="overflow: visible; color: currentcolor;">
+            <path fill-rule="evenodd"
+                  d="m19.85 8.75 4.15.83v4.84l-4.15.83 2.35 3.52-3.43 3.43-3.52-2.35-.83 4.15H9.58l-.83-4.15-3.52 2.35-3.43-3.43 2.35-3.52L0 14.42V9.58l4.15-.83L1.8 5.23 5.23 1.8l3.52 2.35L9.58 0h4.84l.83 4.15 3.52-2.35 3.43 3.43-2.35 3.52zm-1.57 5.07 4-.81v-2l-4-.81-.54-1.3 2.29-3.43-1.43-1.43-3.43 2.29-1.3-.54-.81-4h-2l-.81 4-1.3.54-3.43-2.29-1.43 1.43L6.38 8.9l-.54 1.3-4 .81v2l4 .81.54 1.3-2.29 3.43 1.43 1.43 3.43-2.29 1.3.54.81 4h2l.81-4 1.3-.54 3.43 2.29 1.43-1.43-2.29-3.43.54-1.3zm-8.186-4.672A3.43 3.43 0 0 1 12 8.57 3.44 3.44 0 0 1 15.43 12a3.43 3.43 0 1 1-5.336-2.852zm.956 4.274c.281.188.612.288.95.288A1.7 1.7 0 0 0 13.71 12a1.71 1.71 0 1 0-2.66 1.422z"
+                  clip-rule="evenodd"></path>
+        </svg>
+    )
+}
 
 const App: Component = () => {
+
+    console.log(MenuBar)
+    const [checkBox, setCheckBox] = createSignal(false);
+
     return (
         <div class={style.App}>
+            <MenuBar.Container config={{theme: "light"}}>
+                {/*Tabs*/}
+                <MenuBar.Group title={"Files"}>
+                    {/*Basic list item*/}
+                    <MenuBar.ListItem>
+                        Item1
+                    </MenuBar.ListItem>
+                    {/*List item with checkbox*/}
+                    <MenuBar.ListItem type={"checkbox"} checked={checkBox()} onClick={e => setCheckBox(p => !p)}>
+                        Checkbox
+                    </MenuBar.ListItem>
+                    {/*List item with hint label (useful for annotations and key shortcuts)*/}
+                    <MenuBar.ListItem hintLabel={"Ctrl+C"}
+                                      onClick={()=>{navigator.clipboard.writeText("Some Text");alert("Copied!")}}>
+                        Copy
+                    </MenuBar.ListItem>
+                    {/*List item with icon. icon prop accepts JSXElement!*/}
+                    <MenuBar.ListItem type={"icon"} icon={<GearIcon/>} hintLabel={"Ctrl+,"}>
+                        Preferences
+                    </MenuBar.ListItem>
+                    {/*Separator line*/}
+                    <MenuBar.Separator/>
+                    {/*Submenu*/}
+                    <MenuBar.SubGroup title={"Save"}>
+                        <MenuBar.ListItem hintLabel={"Ctrl+S"}>
+                            Overwrite
+                        </MenuBar.ListItem>
+                        {/*Nested submenu*/}
+                        <MenuBar.SubGroup title={"Export As"}>
+                            <MenuBar.ListItem>HTML</MenuBar.ListItem>
+                            {/*More nested submenu*/}
+                            <MenuBar.SubGroup title={"Docs"}>
+                                <MenuBar.ListItem>PDF</MenuBar.ListItem>
+                                <MenuBar.ListItem>Docx</MenuBar.ListItem>
+                                <MenuBar.ListItem>ODT</MenuBar.ListItem>
+                            </MenuBar.SubGroup>
+                            <MenuBar.SubGroup title={"Image"}>
+                                <MenuBar.ListItem>PNG</MenuBar.ListItem>
+                                <MenuBar.ListItem>JPG</MenuBar.ListItem>
+                                <MenuBar.ListItem>SVG</MenuBar.ListItem>
+                            </MenuBar.SubGroup>
+                        </MenuBar.SubGroup>
+                    </MenuBar.SubGroup>
+                </MenuBar.Group>
 
+                <MenuBar.Group title={"Tab2"}>
+                </MenuBar.Group>
+            </MenuBar.Container>
+            <div class={style.main}>
+                <h1>solid-menubar examples</h1>
+            </div>
         </div>
     )
 }
